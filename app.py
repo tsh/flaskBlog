f4816afc0ce8246bd20c1e1890fbb2f211ecb092
Anton Nikulin <dr.tallin@gmail.com>
2014-05-17 21:15:04 +0300

    some index-user action

--- app.py
+++ app.py
@@ -1,20 +1,83 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, session, render_template, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from forms import NameForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this is not a secret'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    body = db.Column(db.String())
+    author = db.relationship('User', backref='posts', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Post {0}>'.format(self.title)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role {0}>'.format(self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.username)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['True'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known = session.get('known', False))
+
 @app.route('/user/<name>')
 def user(name):
-    return '<h2>hello, {0}</h2>'.format(name)
+    user = User.query.filter_by(username=name)
+    if user is None:
+        user = User(username=name)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return "<h1>hello, {0}</h1>".format(user.username)
 
 if __name__ == '__main__':
     app.run(debug=True)
